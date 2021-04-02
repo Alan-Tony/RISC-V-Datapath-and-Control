32350b95e51378e2b8153ea1e6d9d223706aa625
alantonye@gmail.com <alantonye@gmail.com>
2021-04-02 07:58:39 +0530

    Corrections in Execution module

--- work/@a@l@u_64bit/_primary.vhd
+++ work/@a@l@u_64bit/_primary.vhd
@@ -5,7 +5,7 @@ entity ALU_64bit is
         A               : in     vl_logic_vector(63 downto 0);
         B               : in     vl_logic_vector(63 downto 0);
         Operation       : in     vl_logic_vector(3 downto 0);
-        Result          : in     vl_logic_vector(63 downto 0);
+        Result          : out    vl_logic_vector(63 downto 0);
         Overflow        : out    vl_logic;
         Zero            : out    vl_logic
     );
--- work/signextend/_primary.vhd
+++ work/signextend/_primary.vhd
@@ -2,7 +2,7 @@ library verilog;
 use verilog.vl_types.all;
 entity signextend is
     port(
-        extend          : in     vl_logic_vector(11 downto 0);
+        imm             : in     vl_logic_vector(11 downto 0);
         extended        : out    vl_logic_vector(63 downto 0)
     );
 end signextend;
