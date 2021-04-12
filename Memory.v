617c2927b7db0e832f7e4cd79d481ecede570e31
alantonye@gmail.com <alantonye@gmail.com>
2021-04-12 21:43:13 +0530

    Progress in Memory module

--- Memory.v
+++ Memory.v
@@ -5,12 +5,15 @@ module Memory (clk, Mem_to_Reg_recieve, Mem_Read_recieve, Mem_Write_recieve, Bra
     input clk, Mem_to_Reg_recieve, Mem_Read_recieve, Mem_Write_recieve, Branch_recieve, Zero_recieve;
     input [63:0] result_recieve, writeData_recieve;
 
-
-    //output reg []
+    reg Mem_Read, Mem_Write, Branch, Zero
+    
+    output reg Mem_to_Reg;
+    output reg [63:0] read_data, result;
+    output Mem_to_Reg, PC_SRC;
 
        
 
-    Data_Memory DM(.clk (clk), MemRead, MemWrite, address, WriteData, ReadData);
+    Data_Memory DM(.clk (clk), .MemRead (), MemWrite, address, WriteData, ReadData);
 
 
     always @(posedge clk)
